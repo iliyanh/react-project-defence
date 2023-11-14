64c0b31ee34b348fc9b1f4053889c8917f188943
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-11-14 21:10:33 +0000

    move navigation in components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
-      {/* Navbar */}
-      <header className="header">
-        <a href="/" className="logo">Logo</a>
-
-        <nav className="navbar">
-          <a href="/">Home</a>
-          <a href="/catalog">Catalog</a>
-          <a href="/create">Create</a>
-          <a href="/login">Login</a>
-          <a href="/register">Register</a>
-          <a href="/logout">Logout</a>
-        </nav>
-
-      </header>
+      <Navigation />
       
       
     </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+function Navigation() {
+    return (
+
+        <header className="header">
+            <a href="/" className="logo">Logo</a>
+
+            <nav className="navbar">
+                <a href="/">Home</a>
+                <a href="/catalog">Catalog</a>
+                <a href="/create">Create</a>
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
+                <a href="/logout">Logout</a>
+            </nav>
+
+        </header>
+    )
+}
+export default Navigation
