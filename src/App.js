417274b1b923c7ab22adc56df50d57dfecfd85d8
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-11-15 22:03:20 +0000

    Add Login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 import Navigation from "./components/Navigation";
-import { Route,Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 
 import { Home } from "./components/Home";
 import { Catalog } from "./components/Catalog";
@@ -12,21 +13,23 @@ import { Logout } from "./components/Logout";
 import { Register } from "./components/Register";
 
 function App() {
-  return (
-    <div>
-      <Navigation />
+    return (
+        <div>
+            <Navigation />
 
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/catalog" element={<Catalog/>}/>
-          <Route path="/create" element={<Create/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/logout" element={<Logout/>}/>
-          <Route path="/register" element={<Register/>}/>
-        </Routes>
-      
-    </div>
-        );
+            <main id="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
+
+        </div >
+    );
 }
 
-        export default App;
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom"
+
 export const Login = () => {
-    return <h2>Login</h2>
+    return (
+        <div className="wrapper">
+                <div className="form-box login">
+                    <h2>Login</h2>
+                    <form action="#">
+                        <div className="input-box">
+                            <span className="icon"><ion-icon name="mail"></ion-icon></span>
+                            <input type="email" required></input>
+                            <label>Email</label>
+                        </div>
+                        <div className="input-box">
+                            <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                            <input type="password" required></input>
+                            <label>Password</label>
+                        </div>
+                        <button type="submit" className="btn">Login</button>
+                        <div className="login-register">
+                            <p>Don't have an accout? <Link
+                            to="#"
+                            className="register-link">Register</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    )
 }
