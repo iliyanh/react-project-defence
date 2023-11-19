75232e48bfe433b2dcbe7046c3cfd6385e9f591e
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-11-19 20:00:19 +0000

    add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,18 @@
 export const Home = () => {
-    return <h2>Home</h2>
+    return (
+        <>
+            <div className="wrapper-home">
+                <section className="home">
+                    <div className="home-content">
+                        <h1>Your Number One Place for Real Estate Excellence</h1>
+                        
+                        <h3>Join the countless individuals who have made us their top choice for buying and selling properties.Your dream home awaits—begin your search today.</h3>
+                        
+                        <button type="submit" className="btn-explore">Explore</button>
+                        
+                    </div>
+                </section>
+            </div>
+        </>
+    )
 }
