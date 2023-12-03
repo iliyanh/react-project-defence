0195e0bc2b22226103d3cae737e17b08edafa25a
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-12-03 14:23:18 +0000

    add change value for edit page

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -11,9 +11,13 @@ export const useForm = (initialValues, onSubmitHandler) => {
         
         onSubmitHandler(values);
     }
+    const changeValues = (newValues) => {
+        setValues(newValues)
+    }
     return {
         values,
         changeHandler,
         onSubmit,
+        changeValues
     }
 }
